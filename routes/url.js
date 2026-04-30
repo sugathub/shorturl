f38ef5480ea4fe6b58912acdb362676e1f09bf0b
sugat jmgade <jamgdesugat42@gmail.com>
2026-04-30 16:17:27 +0530

    feat: create URL routes for shortening endpoint

---
+++ routes/url.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { handleGenerateNewShortURL } = require('../controller/url');
+
+const router = express.Router();
+
+router.post('/', handleGenerateNewShortURL);
+
+module.exports = router;
