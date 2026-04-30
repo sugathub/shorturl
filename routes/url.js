5143239215a07e766f0dbd925fdc1efd6125c79a
sugat jmgade <jamgdesugat42@gmail.com>
2026-04-30 17:16:06 +0530

    feat: add analytics route /url/analytics/:shortId

--- routes/url.js
+++ routes/url.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { handleGenerateNewShortURL } = require('../controller/url');
+const { handleGenerateNewShortURL, handleGetAnalytics } = require('../controller/url');
 
 const router = express.Router();
 
 router.post('/', handleGenerateNewShortURL);
+
+router.get('/analytics/:shortId',handleGetAnalytics)
 
 module.exports = router;
